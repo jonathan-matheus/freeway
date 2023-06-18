8e2ca398198618a49496daa69690dcbe5241c578
jonathan-matheus <johhn.dev@gmail.com>
2023-06-18 16:09:15 -0300

    personagem, movimentação

--- sketch.js
+++ sketch.js
@@ -1,6 +1,13 @@
+// variaveis de imagens
 let imagenDaEstrada;
 let imagenDoAtor;
 let imagenCarro;
+
+// variaveis do carro
+let xCarro = 600;
+
+// variaveis do ator
+let yAtor = 366;
 
 function preload(){
   imagenDaEstrada = loadImage("materiais/imagens/estrada.png");
@@ -14,6 +21,49 @@ function setup() {
 
 function draw() {
   background(imagenDaEstrada);
-  image(imagenDoAtor, 100, 366, 30, 30);
-  image(imagenCarro, 420, 40, 50, 40);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+}
+
+/**
+ * Exibe uma imagem de um ator em uma localização específica no canvas.
+ *
+ * @return {void} Esta função não retorna nenhum valor.
+ */
+function mostraAtor(){
+  image(imagenDoAtor, 100, yAtor, 30, 30);
+}
+
+/**
+ * Exibe uma imagem de um carro em uma localização específica no canvas.
+ *
+ * @return {void} Esta função não retorna um valor.
+ */
+function mostraCarro(){
+  image(imagenCarro, xCarro, 40, 50, 40);
+}
+
+/**
+ * Move o carro para a esquerda, diminuindo sua posição x em 2.
+ *
+ * @return {void} Esta função não retorna nada.
+ */
+function movimentaCarro(){
+  xCarro -= 2;
+}
+
+/**
+ * Move o personagem usando as setas do teclado
+ *
+ * @return {void} Não há valor de retorno.
+ */
+function movimentaAtor(){
+  if (keyIsDown(UP_ARROW)){
+    yAtor -= 3;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    yAtor += 3;
+  }
 }
