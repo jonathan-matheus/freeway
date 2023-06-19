4a1997d04db6a9f7195aa11a075e379459cefc4b
jonathan-matheus <johhn.dev@gmail.com>
2023-06-18 21:52:54 -0300

    carros, imagens

--- carro.js
+++ carro.js
@@ -1,6 +1,17 @@
-// variaveis do carro
+// variaveis do carro 1
 let xCarro = 600;
 let yCarro = 40;
+let velocidadeCarro = 2;
+
+// variaveis do carro 2
+let xCarro2 = 600;
+let yCarro2 = 96;
+let velocidadeCarro2 = 3;
+
+// variavel do carro 3
+let xCarro3 = 600;
+let yCarro3 = 150;
+let velocidadeCarro3 = 2.5;
 
 /**
  * Exibe uma imagem de um carro em uma localização específica no canvas.
@@ -9,6 +20,8 @@ let yCarro = 40;
  */
 function mostraCarro(){
   image(imagenCarro, xCarro, yCarro, 50, 40);
+  image(imagenCarro2, xCarro2, yCarro2, 50, 40);
+  image(imagenCarro3, xCarro3, yCarro3, 50, 40);
 }
 
 /**
@@ -17,5 +30,7 @@ function mostraCarro(){
  * @return {void} Esta função não retorna nada.
  */
 function movimentaCarro(){
-  xCarro -= 2;
+  xCarro -= velocidadeCarro;
+  xCarro2 -= velocidadeCarro2;
+  xCarro3 -= velocidadeCarro3;
 }
--- imagens.js
+++ imagens.js
@@ -2,9 +2,13 @@
 let imagenDaEstrada;
 let imagenDoAtor;
 let imagenCarro;
+let imagenCarro2;
+let imagenCarro3;
 
 function preload(){
   imagenDaEstrada = loadImage("materiais/imagens/estrada.png");
   imagenDoAtor = loadImage("materiais/imagens/ator-1.png");
   imagenCarro = loadImage("materiais/imagens/carro-1.png");
+  imagenCarro2 = loadImage("materiais/imagens/carro-2.png");
+  imagenCarro3 = loadImage("materiais/imagens/carro-3.png");
 }
